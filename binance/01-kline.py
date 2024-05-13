d84b80dbe2be5fcb14b4d5713f0de2ebbb56fad9
morteza <mortezamatbou91@gmail.com>
2024-05-13 15:09:14 +0330

    convert response to json

--- binance/01-kline.py
+++ binance/01-kline.py
@@ -9,6 +9,6 @@ endTime = '1715599800000'
 
 url_spot = f'https://api.binance.com/api/v3/klines?symbol={market}&interval={tick_interval}&limit={limit}&startTime={startTime}&endTime={endTime}'
 url_future = f'https://fapi.binance.com/fapi/v1/klines?symbol={market}&interval={tick_interval}&limit={limit}&startTime={startTime}&endTime={endTime}'
-data = requests.get(url_future).content
+data = requests.get(url_future).json()
 
 print(data)
