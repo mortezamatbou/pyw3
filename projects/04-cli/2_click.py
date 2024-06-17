51d2730f1f5a36c6c221e92f859442446388c779
Morteza Matbou <mortezamatbou91@gmail.com>
2024-06-17 18:25:55 +0330

    refactor click to group command

--- projects/04-cli/2_click.py
+++ projects/04-cli/2_click.py
@@ -1,6 +1,11 @@
 import click
 
 
+@click.version_option("1.0.0", prog_name="Digging Sync")
+@click.group()
+def cli():
+    pass
+
 @click.command()
 @click.argument('name')
 @click.option('--age', '-a')
@@ -8,5 +13,16 @@ def main(name, age):
     click.echo("{}, {}".format(name, age))
 
 
+@click.command()
+@click.option('--exchange', '-e', default='all', help='list of symbols for exchange or all symbols')
+@click.option('--test', '-t', help='Prompt test', prompt="Enter your name")
+def pairs(exchange, test):
+    click.echo(f"Pairs for {exchange} exchange")
+    click.echo(f"Prompt input: {test}")
+
+
+cli.add_command(main)
+cli.add_command(pairs)
+
 if __name__ == "__main__":
-    main()
+    cli()
